613d3a8d74bb1eac9f9dc97af04c895803f0daeb
Alisa Kotiun <s20873@pjwstk.edu.pl>
2021-12-13 11:51:56 +0100

    driver-license relationship added

--- src/truckable/Driver.java
+++ src/truckable/Driver.java
@@ -1,4 +1,60 @@
 package truckable;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Driver {
+    private String firstName;
+    private String lastName;
+    private Date dateOfBirth;
+
+    private List<ObtainingData> obtainingDataList;
+
+    public Driver(String firstName, String lastName, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        obtainingDataList = new ArrayList<>();
+    }
+
+    public void addObtainingData(ObtainingData obtainingData) {
+        this.obtainingDataList.add(obtainingData);
+    }
+
+    public String monitor(){
+        return "Driver's location";
+    }
+
+    public String reportOnAnomalyBehavior(){
+        return "Anomaly behavior is detected";
+    }
+
+    public void addDriver(){}
+
+    public void deleteDriver(){}
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
 }
--- src/truckable/License.java
+++ src/truckable/License.java
@@ -1,4 +1,29 @@
 package truckable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class License {
+    private final String type;
+    private final String name;
+
+    private List<ObtainingData> obtainingDataList;
+
+    public License(String type, String name){
+        this.type = type;
+        this.name = name;
+        obtainingDataList = new ArrayList<>();
+    }
+
+    public void addObtainingData(ObtainingData obtainingData) {
+        this.obtainingDataList.add(obtainingData);
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
